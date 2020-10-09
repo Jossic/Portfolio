64e44c7cab829d44f04301be3510c3e98022a75d
Jossic <jossic.lapierre@gmail.com>
2020-10-09 22:00:49 +0200

    Portfolio - Media Queries et Animation de la Navbar

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const btn = document.querySelector('.btn');
+const nav = document.querySelector('.nav-gauche');
+const allItemNav = document.querySelectorAll('.nav-menu-item');
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('active');
+    nav.classList.toggle('menu-visible');
+
 })
+
+if (window.matchMedia('max-width: 1300px')) {
+    allItemNav.forEach((item) => {
+        item.addEventListener('click', () => {
+            btn.classList.toggle('active');
+            nav.classList.toggle('menu-visible');
+        })
+    })
+}
