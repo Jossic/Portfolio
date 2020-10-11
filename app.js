49b761f30e3e41ea9ed404e4e52772bdf29f7032
Jossic <jossic.lapierre@gmail.com>
2020-10-11 21:13:46 +0200

    Portfolio - Animation JS Accueil

--- app.js
+++ app.js
@@ -16,3 +16,29 @@ if (window.matchMedia('max-width: 1300px')) {
         })
     })
 }
+
+
+// Animation écritures
+
+const txtAnim = document.querySelector('.txt-animation');
+
+let typewriter = new Typewriter(txtAnim, {
+    loop: false,
+    deleteSpeed: 20
+})
+
+typewriter
+    .pauseFor(1800)
+    .changeDelay(50)
+    .typeString(`Bonjour, je m'appelle Jossic,  `)
+    .pauseFor(300)
+    .typeString(`je suis <strong> Développeur HTML</strong> !`)
+    .pauseFor(1000)
+    .deleteChars(6)
+    .pauseFor(400)
+    .typeString(`<span style="color: #27ae60">CSS</span> !`)
+    .pauseFor(1000)
+    .deleteChars(5)
+    .pauseFor(400)
+    .typeString(`<span style="color: #ff8300">Full-Stack JS (MERN Stack)</span> !`)
+    .start()
